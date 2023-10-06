118d06ba9bcadc47b82635d43955b939c4583416
Irfandyz <irfandyzize@gmail.com>
2023-10-06 10:13:36 +0700

    Setting Migrations & Models

--- database/migrations/2023_10_06_024251_create_flash_sales_table.php
+++ database/migrations/2023_10_06_024251_create_flash_sales_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('flash_sales', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role'.['user','admin']);
-            $table->rememberToken();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->integer('price');
+            $table->enum('type',['percentage','amount']);
+            $table->integer('amount');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('flash_sales');
     }
 };
--- database/migrations/2023_10_06_024926_create_orders_table.php
+++ database/migrations/2023_10_06_024926_create_orders_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role'.['user','admin']);
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('code');
+            $table->integer('total_price');
+            $table->string('payment');
+            $table->string('expedition');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
